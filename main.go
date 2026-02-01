27cb0dca48479ae9e7c3246218e693fd37f1b491
Housei Ukaji <git@o3o.design>
2026-02-02 00:20:26 +0900

    Fix: handle '-- command' to run the command
    - 'kiromon -- cmd args' now runs cmd with args
    - 'kiromon --' alone still shows usage

--- main.go
+++ main.go
@@ -54,10 +54,14 @@ func main() {
 		return
 	}
 
-	// Handle bare "--" as first argument (no command specified)
+	// Handle "--" as first argument
 	if os.Args[1] == "--" {
-		printUsage()
-		os.Exit(1)
+		if len(os.Args) < 3 {
+			printUsage()
+			os.Exit(1)
+		}
+		runWrapper(os.Args[2:], nil)
+		return
 	}
 
 	// Default: run wrapper mode
